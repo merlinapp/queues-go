3f0fa6da955f70f1a7f80c1f6e1cb44e99c7c396
Eric <eric.david2333@gmail.com>
2020-02-18 08:02:48 -0500

    feat: Event and Metadata structure definitions

---
+++ event.go
@@ -0,0 +1,28 @@
+package queuesgo
+
+import (
+	"encoding/json"
+)
+
+type Event struct {
+	Payload  map[string]interface{} `json:"payload"`  // JSON body of the actual message
+	Metadata EventMetadata          `json:"metadata"` // Event info
+}
+
+type EventMetadata struct {
+	UserID        string `json:"user_id"`        // Id of the user triggering the event
+	CorrelationID string `json:"correlation_id"` // Unique ID of the event, generated as is triggered the first time
+	EventName     string `json:"event_name"`     // event name
+	Origin        string `json:"origin"`         // Microservice originating the event
+	Timestamp     int64  `json:"timestamp"`      // Moment of the event generation
+	ObjectID      string `json:"object_id"`      // ID of the object changing on the event
+}
+
+func (em *EventMetadata) IsZero() bool {
+	return em.CorrelationID == "" || em.EventName == "" || em.Timestamp == 0 || em.ObjectID == "" || em.Origin == ""
+}
+
+func (t Event) String() string {
+	payload, _ := json.Marshal(t)
+	return string(payload)
+}
