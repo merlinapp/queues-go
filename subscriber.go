e09a5290054d5c3a17b010b0b162e53593e4ec39
Eric <eric.david2333@gmail.com>
2020-02-19 11:05:13 -0500

    feat: Document subscribe interface

--- subscriber.go
+++ subscriber.go
@@ -5,8 +5,20 @@ import (
 )
 
 type Subscriber interface {
+	/*
+		Register the function to ve available on the subscription
+		eventName: event that needs to be handled
+		handler: the handler function that will be called when an event with the given name is the eventName given
+	*/
 	RegisterFunction(eventName string, handler HandlerFunc) error
+	/*
+		Blocks the current go routine to wait for events on the subscription name given on the chosen implementation
+	*/
 	Subscribe(ctx context.Context) error
 }
 
-type HandlerFunc func(context.Context, Event) error
+/*
+Function that will handle the Event received, it should return if the message should be acknowledge to the queue
+provider, true to ack, false to indicate a eventual resend (The resend will depend of the subscriber implementation)
+*/
+type HandlerFunc func(context.Context, Event) (bool, error)
