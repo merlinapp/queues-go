b692ad1f6eb1b23b72381ceb454bc6d4c552611a
Eric <eric.david2333@gmail.com>
2020-05-02 19:38:39 -0500

    feat: Add kafka publisher implementation

--- kafka/publisher.go
+++ kafka/publisher.go
@@ -2,11 +2,19 @@ package kafka
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"github.com/dangkaka/go-kafka-avro"
 	queuesgo "github.com/merlinapp/queues-go"
+	"log"
 	"reflect"
+	"time"
 )
 
 type publisher struct {
+	producer   *kafka.AvroProducer
+	topic      string
+	schema     string
 	objectType reflect.Type
 }
 
@@ -17,19 +25,57 @@ the objectType interface should be any of the following types, any other type wi
 2. Non-nil pointer to a struct of the expected type.
 3. A map with key string and any value
 */
-func NewPublisher(project, topic string, objectType interface{}) queuesgo.Publisher {
+func NewPublisher(kafkaServerAddress, schemaServerAddress, topic string, objectType interface{}) queuesgo.Publisher {
 	if !queuesgo.ValidateType(objectType) {
 		return nil
 	}
+	//TODO: Add schema string from the objectType
+	producer, err := kafka.NewAvroProducer([]string{kafkaServerAddress}, []string{schemaServerAddress})
+	if err != nil {
+		log.Printf("Could not create avro producer: %s", err)
+		return nil
+	}
 	return &publisher{
+		producer:   producer,
+		topic:      topic,
 		objectType: reflect.TypeOf(objectType),
 	}
 }
 
 func (p *publisher) PublishSync(ctx context.Context, event *queuesgo.Event) (string, error) {
-	panic("implement me")
+	data, err := p.eventToKafka(event)
+	if err != nil {
+		return "", err
+	}
+	key := time.Now().String()
+	err = p.producer.Add(p.topic, p.schema, []byte(key), data)
+	return key, err
 }
 
 func (p *publisher) PublishAsync(ctx context.Context, event *queuesgo.Event) (<-chan queuesgo.PublicationResult, error) {
-	panic("implement me")
+	data, err := p.eventToKafka(event)
+	if err != nil {
+		return nil, err
+	}
+	res := make(chan queuesgo.PublicationResult, 1)
+	go func() {
+		key := time.Now().String()
+		err = p.producer.Add(p.topic, p.schema, []byte(key), data)
+		p := queuesgo.PublicationResult{Result: key, Err: err}
+		res <- p
+		close(res)
+	}()
+	return res, err
+}
+
+func (p *publisher) eventToKafka(event *queuesgo.Event) ([]byte, error) {
+	//TODO: Add metadata into the publication
+	if !queuesgo.ValidateRegisteredType(event.Payload, p.objectType) {
+		return nil, errors.New("invalid payload")
+	}
+	data, err := json.Marshal(event.Payload)
+	if err != nil {
+		return nil, errors.New("invalid payload")
+	}
+	return data, nil
 }
