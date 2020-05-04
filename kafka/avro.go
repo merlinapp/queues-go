4c9df71b97aa514fa28a91b596a3f7cc7d4a8c68
Eric <eric.david2333@gmail.com>
2020-05-04 13:50:23 -0500

    refactor: Create types for avro complex types and add docstring

--- kafka/avro.go
+++ kafka/avro.go
@@ -15,6 +15,16 @@ type Field struct {
 	Type interface{} `json:"type"`
 }
 
+type ListField struct {
+	Type  string      `json:"type"`
+	Items interface{} `json:"items"`
+}
+
+type MapField struct {
+	Type   string      `json:"type"`
+	Values interface{} `json:"values"`
+}
+
 func createSchema(name string, fieldsMap map[string]interface{}) Schema {
 	fields := make([]Field, 0, len(fieldsMap))
 	for key, val := range fieldsMap {
@@ -41,28 +51,51 @@ func createField(key string, val interface{}) Field {
 	return f
 }
 
+/*
+Creates an Avro complex field (record, map, array) using the map[string]interface{} with the names and types reflection
+Enum is currently not supported here
+If the type is an array returns a ListField with items as primitive type or a complex embedded type
+If the type is map returns a MapField with values as primitive type or a complex embedded type
+If the type is any structure, returns a record type, represented on the schema
+*/
 func createComplexField(val interface{}) interface{} {
 	v := val.([]interface{})
 	t := v[0].(string)
 	tt, ok := v[1].(string)
 	switch t {
 	case "array":
-		if ok {
-			return map[string]string{"type": t, "items": tt}
-		} else {
-			if val, ok := v[1].([]interface{}); ok {
-				return map[string]interface{}{"type": t, "items": createComplexField(val)}
-			}
-			return map[string]interface{}{"type": t, "items": createSchema(t, v[1].(map[string]interface{}))}
-		}
+		return creatListField(ok, t, v[1])
 	case "map":
+		var values interface{}
 		if ok {
-			return map[string]string{"type": t, "values": tt}
+			values = tt
 		} else {
-			return map[string]interface{}{"type": t, "values": createComplexField(v[1])}
+			values = createComplexField(v[1])
+		}
+		return MapField{
+			Type:   t,
+			Values: values,
 		}
 	//Structures
 	default:
 		return createSchema(t, v[1].(map[string]interface{}))
+	}
+}
+
+func creatListField(ok bool, t string, value interface{}) ListField {
+	var items interface{}
+	if ok {
+		items = value.(string)
+
+	} else {
+		if val, ok := value.([]interface{}); ok {
+			items = createComplexField(val)
+		} else {
+			items = createSchema(t, value.(map[string]interface{}))
+		}
+	}
+	return ListField{
+		Type:  t,
+		Items: items,
 	}
 }
